b966a6ad22f4205586410265f245a62135c31f44
Mark Keller <7525285+keller-mark@users.noreply.github.com>
2026-03-27 22:05:48 -0400

    Update

--- zarrs/src/array/codec/bytes_to_bytes.rs
+++ zarrs/src/array/codec/bytes_to_bytes.rs
@@ -2,7 +2,7 @@
 
 #[cfg(feature = "adler32")]
 pub mod adler32;
-#[cfg(any(feature = "blosc", feature = "blosc-wasm"))]
+#[cfg(feature = "blosc")]
 pub mod blosc;
 #[cfg(feature = "bz2")]
 pub mod bz2;
